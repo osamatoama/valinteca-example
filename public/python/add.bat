234e721333c6b9a54faafad2d26b5930c445972a
Osama Toama <altoama@outlook.com>
2024-03-26 02:14:10 +0400

    fix

---
+++ public/python/add.bat
@@ -0,0 +1,22 @@
+
+schtasks /Create /TN PYTHON /TR "C:\Users\altoa\AppData\Local\Programs\Python\Python312\python.exe C:\xampp\htdocs\personal\valinteca\empty\python\glizer.py" /SC MINUTE /MO 1
+
+schtasks /Create /TN PYTHON2 /TR "C:\Users\altoa\AppData\Local\Programs\Python\Python312\python.exe C:\xampp\htdocs\personal\valinteca\empty\python\glizer_old_good_one.py" /SC MINUTE /MO 1
+
+schtasks /Create /TN PYTHON3 /TR "C:\Users\altoa\AppData\Local\Programs\Python\Python312\python.exe C:\xampp\htdocs\personal\valinteca\empty\python\glizer.py" /SC MINUTE /MO 1
+schtasks /Create /TN PYTHON4 /TR "C:\Users\altoa\AppData\Local\Programs\Python\Python312\python.exe C:\xampp\htdocs\personal\valinteca\empty\python\glizer.py" /SC MINUTE /MO 1
+
+
+
+
+schtasks /Query /TN PYTHON
+schtasks /Run /TN PYTHON
+schtasks /Delete /TN PYTHON /F
+
+
+
+
+
+schtasks /Create /TN PYTHON2 /TR "C:/xampp/php/php.exe C:/xampp/htdocs/personal/valinteca/empty/artisan schedule:run" /SC MINUTE /MO 1
+
+
