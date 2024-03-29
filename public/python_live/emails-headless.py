887c5a759da49f365f5ebcd1449b484616fbbb77
Osama Toama <altoama@outlook.com>
2024-03-29 06:35:31 +0400

    fix

--- public/python_live/emails-headless.py
+++ public/python_live/emails-headless.py
@@ -31,8 +31,17 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = uc.ChromeOptions()
 #chrome_options.add_extension('AdBlock.crx')
 #chrome_options.add_extension('xPath-Finder.crx')
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--ignore-ssl-errors")
+chrome_options.add_argument("no-sandbox")
+
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+
 #chrome_options.add_argument(r"--user-data-dir=C:\Users\Yahya\AppData\Local\Google\Chrome\User Data\Default")
 #chrome_options.add_argument('--profile-directory=Default')
 #chrome_options.add_argument(r"--user-data-dir=C:\Users\yahya\AppData\Local\Google\Chrome\User Data\Default")
@@ -58,7 +67,7 @@ wait = WebDriverWait(browser, 30)  # waits for 10 seconds max
 
 
 login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/input[2]')))
-login_button.send_keys("zyayaolabi2@gmail.com")
+login_button.send_keys("osama222@gmail.com")
 
 
 login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/input[3]')))
