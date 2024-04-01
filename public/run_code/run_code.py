828604eb33d47f9b0fed9d20b44e8c8a111293aa
Osama Toama <altoama@outlook.com>
2024-04-01 04:54:35 +0400

    fix

--- public/run_code/run_code.py
+++ public/run_code/run_code.py
@@ -5,7 +5,8 @@ import os
 url = r'C:\xampp\htdocs\personal\valinteca\empty\public\run_code'
 
 def run():
-    os.system(f"start cmd /k py {url}\\code_every_1.py")
+    os.system(f"start cmd /k py {url}\\code_every_5.py")
+    os.system(f"start cmd /k py {url}\\code_every_10.py")
     # time.sleep(30)
 
 run()
