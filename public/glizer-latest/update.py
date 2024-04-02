bfa86abef1d14d625ff9c24f7abf2688e22e11c4
Osama Toama <altoama@outlook.com>
2024-04-02 11:11:55 +0400

    Update update.py

--- public/glizer-latest/update.py
+++ public/glizer-latest/update.py
@@ -1,7 +1,7 @@
 from requests import get
 
 # Download the file
-code = get("https://example.valinteca.com/run_code/glizer.py").text
+code = get("https://example.valinteca.com/glizer-latest/glizer.py").text
 
 # Write the data to a file
 with open("glizer.py", "w") as f:
