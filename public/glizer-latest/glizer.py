04753ee29e1aafd3c6ba83bfeb5fb42185ce3e29
Osama Toama <altoama@outlook.com>
2024-04-02 11:09:26 +0400

    new code

--- public/glizer-latest/glizer.py
+++ public/glizer-latest/glizer.py
@@ -1,4 +1,19 @@
 
+
+
+
+'''
+
+updates:
+
+    new sucessful has been handled
+    improvment in performance (using exceptions instead of trying each one)
+
+    id wornge handled better
+
+
+'''
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -35,7 +50,10 @@ try:
     PlayerId = r.json()['player_id']
     '''
 
-    headers={"Content-Type":"application/json", "Accept":"application/json","X-Authorization": "HnweAEO5T7SArZCiy5SjzOx9cZ96qGEejaiIkvyZLZW1PrBZX64ofs5lO6s6UCmK","X-Device":"osama-new-code"}
+
+
+    '''
+    headers={"Content-Type":"application/json", "Accept":"application/json","X-Authorization": "HnweAEO5T7SArZCiy5SjzOx9cZ96qGEejaiIkvyZLZW1PrBZX64ofs5lO6s6UCmK","X-Device":"yahya-1-new-code"}
     r = requests.get(url="https://sahwa.valantica.com/api/v1/bot", headers=headers)
     if(r.json()['success'] == False):
         print(r.json())
@@ -45,6 +63,18 @@ try:
 
     code = r.json()['code']
     PlayerId = r.json()['player_id']
+    '''
+    headers={"Content-Type":"application/json", "Accept":"application/json","X-Authorization": "HnweAEO5T7SArZCiy5SjzOx9cZ96qGEejaiIkvyZLZW1PrBZX64ofs5lO6s6UCmK","X-Device":"yahya-h-new-code-2"}
+    r = requests.get(url="https://sahwa.valantica.com/api/v1/bot", headers=headers)
+    if(r.json()['success'] == False):
+        print(r.json())
+        exit()
+    user_name = r.json()['email']
+    password = r.json()['password']
+
+    code = r.json()['code']
+    PlayerId =  r.json()['player_id']
+
 
 
     print(PlayerId)
@@ -98,59 +128,70 @@ try:
     time.sleep(1)
     Change_Player_ID_input.clear()
     Change_Player_ID_input.send_keys(PlayerId)
-
     Change_Player_ID_ok= wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div[1]/div/div[3]/div/div/div/div')))
     Change_Player_ID_ok.click()
 
 
-    Redemption_code = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/input')))
-    Redemption_code.clear()
-    Redemption_code.send_keys(code)
+    try:
+        Redemption_code = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/input')))
+        Redemption_code.clear()
+        Redemption_code.send_keys(code)
 
 
-    Redemption_code_ok = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div/div/div[2]/div[2]/div/div')))
-    Redemption_code_ok.click()
+        Redemption_code_ok = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div/div/div[2]/div[2]/div/div')))
+        Redemption_code_ok.click()
 
-
+    except:
+        if ('Invalid Game ID' in wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[2]/div[1]/div/div[2]/div[2]/p'))).text) :
+            osama to do   requests.post(url="https://sahwa.valantica.com/api/v1/convert-to-manual",json={"code":code, "note": "ID is wrong", }, headers=headers)
+            print("id is wrong")
+            time.sleep(2)
+            browser.quit()
+            exit()
 
 
 
     try:
         Redemption_code_submit = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[4]/div/div/div/div')))
         Redemption_code_submit.click()
-        Redeem_successfuly_ok = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[3]/div[3]/div/div[3]/div/div/div/div/div')))
-        if Redeem_successfuly_ok.text == 'OK' :
+        if wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[3]/div/div[4]/div/div/div/div/div'))).text =="Return to Shop":
             requests.post(url="https://sahwa.valantica.com/api/v1/redeem",json={"code":code, "email": user_name, "status": "redeemed"}, headers=headers)
-            print("Success ok")
+            print("Success ok  new way")
             time.sleep(2)
             browser.quit()
             exit()
 
+
+
     except Exception as error:
-        print("An exception occurred:", error) #
+        try:
+            if wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[3]/div[3]/div/div[3]/div/div/div/div/div'))).text == 'OK' :
+                requests.post(url="https://sahwa.valantica.com/api/v1/redeem",json={"code":code, "email": user_name, "status": "redeemed"}, headers=headers)
+                print("Success ok")
+                time.sleep(2)
+                browser.quit()
+                exit()
+        except:
+            try:
+                if ( ( wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[1]/div/div'))).text == 'Redemption code is redeemed by someone else') or (wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[1]/div/div'))).text == 'You have already redeemed')):
+                    print("'Redemption code is redeemed by someone else'")
+                    requests.post(url="https://sahwa.valantica.com/api/v1/redeem",json={"code":code, "email": user_name, "status": "redeemed"}, headers=headers)
+                    browser.quit()
+                    exit()
+            except:
+                if wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[5]/div/div/div/div'))).text == 'verify comfirm' :
+                    requests.post(url="https://sahwa.valantica.com/api/v1/block",json={"email":user_name}, headers=headers)
+                    print("block-email")
+                    browser.quit()
+                    exit()
+                print("An exception occurred:", error) #
 
-    try:
-        Redemption_code_submit = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[1]/div/div')))
-        if Redemption_code_submit.text == 'Redemption code is redeemed by someone else' or Redemption_code_submit.text == 'You have already redeemed':
-            print("'Redemption code is redeemed by someone else'")
-            requests.post(url="https://sahwa.valantica.com/api/v1/redeem",json={"code":code, "email": user_name, "status": "redeemed"}, headers=headers)
 
-        else:
-            print("Skip ")
-    except:
-        time.sleep(1)
 
-    try:
-        Redemption_code_submit = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[5]/div[4]/div[2]/div[2]/div[5]/div/div/div/div')))
-        if Redemption_code_submit.text == 'verify comfirm' :
-            requests.post(url="https://sahwa.valantica.com/api/v1/block",json={"email":user_name}, headers=headers)
-            print("block-email")
-    except:
-        time.sleep(1)
+
 
 except Exception as error:
     print("An exception occurred:", error) # An exception occurred:
-    requests.post(url="https://sahwa.valantica.com/api/v1/redeem",json={"code":code, "email": user_name, "status": "open_to_request"}, headers=headers)
     print("CODE HAS NOT REDEEMED")
 
 
--- public/glizer-latest/run_code_test.py
+++ public/glizer-latest/run_code_test.py
@@ -2,7 +2,7 @@
 
 import os
 
-url = r'C:\xampp\htdocs\personal\valinteca\empty\public\run_code'
+url = r'C:\xampp\htdocs\personal\valinteca\empty\public\glizer-latest'
 
 def run():
     os.system(f"start cmd /k py {url}\\code_every_1_test.py")
