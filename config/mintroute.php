325b2f4e8597575b5b34db34934b6f96c27dbd0d
Osama Toama <altoama@outlook.com>
2024-05-13 13:22:16 +0400

    fix

--- config/mintroute.php
+++ config/mintroute.php
@@ -7,6 +7,8 @@ return [
 
     'urls' => [
         'get_current_balance' => 'https://sandbox.mintroute.com/vendor/api/get_current_balance',
+        'brand' => 'https://sandbox.mintroute.com/vendor/api/brand',
+
         //        'user_order_create'     => env('YUQUE_API_BASE_URL', 'http://tkog.qr67.com:8080/api') . env('YUQUE_USER_ORDER_CREATE', '/user-order/create'),
         //        'user_order_details'    => env('YUQUE_API_BASE_URL', 'http://tkog.qr67.com:8080/api') . env('YUQUE_USER_ORDER_DETAILS', '/user-order/details'),
         //        'user_products_list'    => env('YUQUE_API_BASE_URL', 'http://tkog.qr67.com:8080/api') . env('YUQUE_USER_PRODUCTS_LIST', '/user-goods/list'),
