ec0254ab1d3e09db9fedd80a327580cbb34a3544
عَمْـــــرُو <77831717+Amr-Guaily@users.noreply.github.com>
2024-06-23 10:50:50 +0300

    Update4 arabic2.blade.php

--- resources/views/pdf/arabic2.blade.php
+++ resources/views/pdf/arabic2.blade.php
@@ -1,13 +1,16 @@
-<!doctype html>
-<html lang="ar">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ticket 1111111111111</title>
-<style>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Valinteca - ticket</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400..700&display=swap"
+      rel="stylesheet"
+    />
+    <style>
       * {
         padding: 0;
         margin: 0;
@@ -141,10 +144,8 @@
         justify-content: space-between;
       }
     </style>
-</head>
-
-<body>
-
+  </head>
+  <body>
     <table class="wrapper" role="presentation">
       <tr>
         <td class="part-1">
@@ -248,6 +249,5 @@
         </td>
       </tr>
     </table>
-</body>
-
+  </body>
 </html>
