1ae07f797c8e7e8c728f12e6f2d4f6a1557b06f2
عَمْـــــرُو <77831717+Amr-Guaily@users.noreply.github.com>
2024-06-23 10:40:15 +0300

    Update1 arabic2.blade.ph

--- resources/views/pdf/arabic2.blade.php
+++ resources/views/pdf/arabic2.blade.php
@@ -7,71 +7,139 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ticket 1111111111111</title>
-    <style>
-        @page {
-            margin: 0;
-        }
-
-        body {
-            font-family: 'Tajawal', sans-serif;
-        }
-
-        .ticket-text {
-            position: fixed;
-            top: 240px;
-            right: 25px;
-            rotate: 90;
-            text-align: center;
-
-            color: white;
-
-        }
-
-        .ticket-number {
-            position: fixed;
-            top: 40px;
-            right: 25px;
-            rotate: 90;
-            text-align: center;
-
-            color: white;
-        }
-
-        .event-title {
-            position: fixed;
-            top: 40px;
-            right: 100px;
-            text-align: center;
-            z-index: 100;
-        }
-
-        .event-date {
-            position: fixed;
-            top: 250px;
-            right: 100px;
-            padding: 12px 32px;
-            background-color: #f5f0ea;
-            font-weight: 700;
-            text-align: center;
-            word-spacing: 4px;
-            width: 160px;
-        }
-
-        .event-time {
-            position: fixed;
-            top: 40px;
-            right: 400px;
-            text-align: center;
-            z-index: 100;
-        }
-
-        .ticket-price {
-            position: fixed;
-            top: 40px;
-            right: 600px;
-            text-align: center;
-            z-index: 100;
-        }
+<style>
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+      }
+      body {
+        font-family: 'El Messiri', sans-serif;
+        background-color: red;
+        padding: 0 15px;
+        text-align: center;
+        min-height: 100vh;
+      }
+      table {
+        border-collapse: collapse;
+        margin: auto;
+        width: 100%;
+      }
+      .wrapper {
+        width: 100%;
+        max-width: 768px;
+        overflow: hidden;
+        position: relative;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .ellipse {
+        background-color: #1e1e1e;
+        border-radius: 50%;
+      }
+      .ellipse-lg {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+      }
+      .ellipse-sm {
+        width: 30px;
+        height: 30px;
+      }
+      .part-1 {
+        background-color: #222222;
+        color: #fff;
+        position: relative;
+        width: 60px;
+        text-align: center;
+        vertical-align: middle;
+      }
+      .part-1 .ellipse-lg {
+        top: 50%;
+        transform: translateY(-50%);
+        right: -20px;
+        background-color: #1e1e1e;
+      }
+      .part-1 span {
+        display: block;
+        margin: 10px 0;
+        transform: rotate(90deg);
+        white-space: nowrap;
+        margin: 100px 0;
+      }
+      .part-2 {
+        background-color: #fff;
+        color: #000;
+        border-left: 2px dashed #000;
+        padding: 20px;
+        width: 100%;
+      }
+      .main-content h1 {
+        font-size: 38px;
+        line-height: 2.5rem;
+        font-weight: 700;
+        margin-bottom: 1.5rem;
+      }
+      .date {
+        padding: 12px 32px;
+        background-color: #f5f0ea;
+        font-weight: 700;
+        text-align: center;
+        word-spacing: 4px;
+      }
+      .info-item {
+        margin-bottom: 10px;
+      }
+      .info-item span {
+        font-weight: 700;
+        display: block;
+      }
+      .info-item small {
+        font-weight: 600;
+        font-size: 12px;
+        display: block;
+      }
+      .actions img {
+        max-height: 90px;
+        max-width: 80px;
+        object-fit: contain;
+      }
+      .actions tr {
+        height: 150px; /* Adding space between rows */
+      }
+      .part-3 {
+        background-color: #f5f0ea;
+        position: relative;
+        width: 165px;
+        text-align: center;
+        padding-right: 20px;
+      }
+      .part-3 p {
+        font-weight: 700;
+        font-size: 24px;
+        transform: rotate(90deg);
+        margin-left: 25px;
+        margin-top: 20px;
+      }
+      .serial-code img {
+        width: 50px;
+        height: 55px;
+        display: block;
+      }
+      .actions tr {
+        height: 150px;
+      }
+      .left-ellipses {
+        position: absolute;
+        height: 90%;
+        top: 5%;
+        left: -15px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
     </style>
 </head>
 
