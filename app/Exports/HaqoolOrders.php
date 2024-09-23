98b7c7640f94ac0a727175a8b3205593c1f2d52c
Osama Toama <altoama@outlook.com>
2024-09-23 13:07:10 +0400

    Update HaqoolOrders.php

--- app/Exports/HaqoolOrders.php
+++ app/Exports/HaqoolOrders.php
@@ -7,7 +7,6 @@ use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
 
 class HaqoolOrders implements FromCollection, ShouldAutoSize, WithHeadings
 {
@@ -22,31 +21,34 @@ class HaqoolOrders implements FromCollection, ShouldAutoSize, WithHeadings
 
     public function collection()
     {
-        return HaqoolOrder::orderBy('order_date', 'ASC')->get();
+        return HaqoolOrder::select('product_name', 'sku', 'brand', 'cost', 'quantity', 'total', 'order_number',
+            'order_date', 'order_status', 'client_name', 'client_email', 'client_phone', 'client_city',
+            'payment_method', 'invoice_number', 'salla_order_id')->orderBy('order_date', 'ASC')->get();
 
     }
-/*
-    public function map($order): array
-    {
-        return [
-            $order['product_name'],
-            $order['sku'],
-            $order['brand'],
-            $order['cost'],
-            $order['quantity'],
-            $order['total'],
-            $order['order_number'],
-            $order['order_date'],
-            $order['order_status'],
-            $order['client_name'],
-            $order['client_email'],
-            $order['client_phone'],
-            $order['client_city'],
-            $order['payment_method'],
-            $order['invoice_number'],
 
-        ];
-    }*/
+    /*
+        public function map($order): array
+        {
+            return [
+                $order['product_name'],
+                $order['sku'],
+                $order['brand'],
+                $order['cost'],
+                $order['quantity'],
+                $order['total'],
+                $order['order_number'],
+                $order['order_date'],
+                $order['order_status'],
+                $order['client_name'],
+                $order['client_email'],
+                $order['client_phone'],
+                $order['client_city'],
+                $order['payment_method'],
+                $order['invoice_number'],
+
+            ];
+        }*/
 
     public function headings(): array
     {
